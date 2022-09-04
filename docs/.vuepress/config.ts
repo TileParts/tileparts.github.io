57ab08505fc6a1aac6ddacb98ce58c9efae0174f
XiNGRZ <hi@xingrz.me>
2022-09-04 23:17:26 +0800

    Fix html lang

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vuepress/config';
 
 export default defineConfig(ctx => ({
-  title: 'TILE',
-  description: '模块化硬件原型开发方案',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'TILE',
+      description: '模块化硬件原型开发方案',
+    },
+  },
 }));
